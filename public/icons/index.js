dfbc3a36e491e01f9be70bb59ecd68e5b5e6f3fc
renvelvet <reshapuspita734@gmail.com>
2023-05-26 01:26:03 +0700

    feat: statis header

---
+++ public/icons/index.js
@@ -0,0 +1,8 @@
+import forward from './forward.svg'
+import facebook from './facebook.svg'
+import tiktok from './tiktok.svg'
+import youtube from './youtube.svg'
+import instagram from './instagram.svg'
+import twitter from './twitter.svg'
+
+export { forward, facebook, twitter, tiktok, instagram, youtube }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,19 @@
+import {
+  facebook,
+  forward,
+  instagram,
+  tiktok,
+  twitter,
+  youtube,
+} from '../../public/icons'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
+import Image from 'next/image'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin'],
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +23,66 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>
+        <nav className="w-full h-10 fixed bg-[#F9F9F9] border-b-blue-primary">
+          <div className="flex max-w-6xl h-full mx-auto flex-row justify-between items-center">
+            <ul className="flex flex-row items-center gap-x-[13px] h-fit text-[#272121] text-sm font-medium leading-4">
+              <li>RCTI+</li>
+              <li>Vision+</li>
+              <li>Okezone.com</li>
+              <li>SINDOnews.com</li>
+              <li>iNews.id</li>
+              <li>Buddyku</li>
+              <li>
+                <Image src={forward} alt="dropdown icon" width={9} height={6} />
+              </li>
+            </ul>
+            <ul className="flex flex-row items-center gap-x-[15px] h-fit">
+              <li>
+                <Image
+                  src={facebook}
+                  alt="facebook-icon"
+                  weight={29}
+                  height={29}
+                />
+              </li>
+              <li>
+                <Image
+                  src={twitter}
+                  alt="facebook-icon"
+                  weight={29}
+                  height={29}
+                />
+              </li>
+              <li>
+                <Image
+                  src={instagram}
+                  alt="facebook-icon"
+                  weight={29}
+                  height={29}
+                />
+              </li>
+              <li>
+                <Image
+                  src={youtube}
+                  alt="facebook-icon"
+                  weight={29}
+                  height={29}
+                />
+              </li>
+              <li>
+                <Image
+                  src={tiktok}
+                  alt="facebook-icon"
+                  weight={29}
+                  height={29}
+                />
+              </li>
+            </ul>
+          </div>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }
